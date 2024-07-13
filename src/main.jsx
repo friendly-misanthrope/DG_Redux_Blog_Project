059c676cb01e47caea3be5f44bf8e8749dd06fd1
friendly-misanthrope <nickthedevguy@gmail.com>
2024-07-12 23:36:47 -0700

    Installed & setup React Router

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from './App.jsx'
 import { store } from './app/store.js'
 import { Provider } from 'react-redux'
 import { fetchUsers } from './features/users/usersSlice.js'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
 
 store.dispatch(fetchUsers());
@@ -11,6 +12,11 @@ store.dispatch(fetchUsers());
 ReactDOM.createRoot(document.getElementById('root')).render(
   
     <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/*' element={<App />} />
+        </Routes>
+      </BrowserRouter>
       <App />
     </Provider>
   
