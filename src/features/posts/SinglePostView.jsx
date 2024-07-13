c1594d3b760a55d708fabb24c2f04931c1d53360
friendly-misanthrope <nickthedevguy@gmail.com>
2024-07-12 23:18:34 -0700

    created component file for viewing a full post, declared imports

---
+++ src/features/posts/SinglePostView.jsx
@@ -0,0 +1,13 @@
+import { useSelector } from "react-redux";
+import AuthorView from "./AuthorView";
+import CreatedAtView from "./CreatedAtView";
+import ReactionButtonsView from "./ReactionButtonsView";
+import { selectPostById } from "./postsSlice";
+
+const SinglePostView = () => {
+  return (
+    <div>SinglePostView</div>
+  )
+}
+
+export default SinglePostView;
