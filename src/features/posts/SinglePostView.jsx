b2541a91c7e3659274f739d97bce619db870d75e
friendly-misanthrope <nickthedevguy@gmail.com>
2024-07-12 23:28:12 -0700

    added conditionally rendered JSX for full post

--- src/features/posts/SinglePostView.jsx
+++ src/features/posts/SinglePostView.jsx
@@ -1,12 +1,30 @@
 import { useSelector } from "react-redux";
+import { selectPostById } from "./postsSlice";
 import AuthorView from "./AuthorView";
 import CreatedAtView from "./CreatedAtView";
 import ReactionButtonsView from "./ReactionButtonsView";
-import { selectPostById } from "./postsSlice";
 
 const SinglePostView = () => {
+  // todo: get post ID from useParams()
+  const postById = useSelector(selectPostById(state, postId));
+
+  if (!postById) {
+    return (
+      <section>
+        <h2>The specified post does not exist</h2>
+      </section>
+    )
+  }
   return (
-    <div>SinglePostView</div>
+    <article>
+      <h2>{post.title}</h2>
+      <p>{post.body}</p>
+      <p className="postCredit">
+        <AuthorView userId={post.userId} />
+        <CreatedAtView timestamp={post.createdAt} />
+      </p>
+      <ReactionButtonsView post={post} />
+    </article>
   )
 }
 
