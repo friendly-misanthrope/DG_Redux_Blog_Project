5b3505cfbcdcf9e3193110985ef89ae75505bdf4
friendly-misanthrope <nickthedevguy@gmail.com>
2024-07-12 23:58:02 -0700

    Setup React routes for viewing all posts, adding a post, and viewing a full post

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 // import CSS and components here
 import PostsView from "./features/posts/PostsView";
 import AddPostView from "./features/posts/AddPostView";
+import SinglePostView from "./features/posts/SinglePostView";
+import Layout from './components/Layout'
+import { Routes, Route } from 'react-router-dom';
 
 const App = () => {
   return (
-    <div className='App'>
-      <AddPostView />
-      <PostsView />
-    </div>
+    <Routes>
+      <Route path='/' element={<Layout />} />
+        <Route index element={<PostsView />} />
+        <Route path='post'>
+          <Route index element={<AddPostView />} />
+          <Route path=":postId" element={<SinglePostView />} />
+        </Route>
+    </Routes>
   );
 }
 
