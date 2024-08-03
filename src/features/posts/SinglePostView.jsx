af44bb9056458f92ecc399d847783e8820913e23
friendly-misanthrope <nickthedevguy@gmail.com>
2024-08-02 23:01:09 -0700

    Added component for viewing one full post

---
+++ src/features/posts/SinglePostView.jsx
@@ -0,0 +1,32 @@
+import { useSelector } from 'react-redux';
+import { selectPostById } from './postsSlice';
+
+import AuthorView from './AuthorView';
+import CreatedAtView from './CreatedAtView';
+import ReactionButtonsView from './ReactionButtonsView';
+
+const SinglePostView = () => {
+  const post = useSelector((state) => selectPostById(state, postId));
+
+  if (!post) {
+    return (
+      <section>
+        <h2>The specified post doesn't exist</h2>
+      </section>
+    );
+  }
+
+  return (
+    <article>
+      <h2>{post.title}</h2>
+      <p>{post.body}</p>
+      <p className="postCredit">
+        <AuthorView userId={post.userId} />
+        <CreatedAtView timestamp={post.createdAt} />
+      </p>
+      <ReactionButtonsView post={post}/>
+    </article>
+  );
+}
+
+export default SinglePostView;
