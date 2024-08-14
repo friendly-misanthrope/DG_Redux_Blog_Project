3305383bb567307c16a6f5aec78c545bbfba2347
friendly-misanthrope <nickthedevguy@gmail.com>
2024-08-13 21:57:05 -0700

    implemented React Route for App component

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import { store } from './app/store.js';
 import { Provider } from 'react-redux';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { fetchUsers } from './features/users/usersSlice'
 import './index.css';
 
@@ -10,7 +11,11 @@ import './index.css';
 store.dispatch(fetchUsers());
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/*" element={<App />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
 );
