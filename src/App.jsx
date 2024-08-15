e0a911fb13c696011004528af2043c24c3217d02
friendly-misanthrope <nickthedevguy@gmail.com>
2024-08-14 18:02:12 -0700

    Implemented routing for adding a new post and viewing one full post

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import AddPostView from "./features/posts/AddPostView";
 import PostsView from "./features/posts/postsView";
+import SinglePostView from "./features/posts/SinglePostView";
+import Layout from './components/Layout';
+import { Route, Routes } from "react-router-dom";
 
 const App = () => {
   return (
-    <main className="App">
-      <AddPostView />
-      <PostsView />
-    </main>
+    <Routes>
+      <Route path='/' element={<Layout />}>
+        <Route index element={<PostsView />} />
+        <Route path='post'>
+          <Route index element={<AddPostView />} />
+          <Route path=':postId' element={<SinglePostView />} />
+        </Route>
+      </Route>
+    </Routes>
   );
 }
 
