6e85abc9554fcede4ce8be28d261a1c8b9f6b82c
friendly-misanthrope <nickthedevguy@gmail.com>
2024-08-18 22:07:25 -0700

    added users link to nav menu, added css rules for 'users' ordered/unordered list links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,6 +8,7 @@ const Header = () => {
         <ul>
           <li><Link to={'/'}>Home</Link></li>
           <li><Link to={'post'}>New Post</Link></li>
+          <li><Link to={'users'}>Users</Link></li>
         </ul>
       </nav>
     </header>
