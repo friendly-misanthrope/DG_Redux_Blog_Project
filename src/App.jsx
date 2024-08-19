1f623fe3e706b64825c48f0edf2048e52db1df56
friendly-misanthrope <nickthedevguy@gmail.com>
2024-08-19 00:03:09 -0700

    added catch-all route

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,14 @@ import Layout from "./components/Layout";
 import EditPostView from "./features/posts/EditPostView";
 import UsersView from "./features/users/UsersView";
 import SingleUserView from "./features/users/SingleUserView";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<PostsView />} />
+
         <Route path="post">
           <Route index element={<AddPostView />} />
           <Route path=":postId" element={<SinglePostView />} />
@@ -22,6 +23,8 @@ const App = () => {
           <Route index element={<UsersView />} />
           <Route path=':userId' element={<SingleUserView />} />
         </Route>
+
+        <Route path='*' element={<Navigate to='/' replace />} />
       </Route>
     </Routes>
   );
